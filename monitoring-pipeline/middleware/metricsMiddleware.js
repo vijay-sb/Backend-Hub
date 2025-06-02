9e634cf1d4d28d67a903bb5860f1444420cd0d9f
Vijay S B <153985965+vijay-sb@users.noreply.github.com>
2025-06-02 17:19:54 +0530

    Update metricsMiddleware.js

--- monitoring-pipeline/middleware/metricsMiddleware.js
+++ monitoring-pipeline/middleware/metricsMiddleware.js
@@ -1,19 +1,14 @@
 const { HttpRequestCounter, ResponseTimeHistogram } = require('../metrics/prometheus.js');
 
 module.exports = function metricsMiddleware(req, res, next) {
-  // Start timer for response time measurement with method and route labels
+  
   const end = ResponseTimeHistogram.startTimer({ method: req.method, route: req.path });
 
-  // Listen for the 'finish' event on the response to stop the timer and increment counters
   res.on('finish', () => {
-    // Increment the request counter with labels for HTTP method, route, and status code
     HttpRequestCounter.labels(req.method, req.path, res.statusCode).inc();
-
-    // Stop the timer and record the response duration
     end();
   });
 
-  // Proceed to the next middleware
   next();
 };
 
