362a04042f614ffa5c596ef3c890c65fa86f4974
vijaysb0613 <vijaysb2006@gmail.com>
2025-07-18 22:34:12 +0530

    api function

--- microservice/api/main.go
+++ microservice/api/main.go
@@ -8,3 +8,26 @@ import (
 )
 
 var MySigningKey = []byte(os.Getenv("SECRET_KEY"))
+func homepage(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w,"Super Secret Information")
+}
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler{
+  return http.HandleFunc(func(w http.ResponseWriter, r *http.Request){
+  if r.Header("Token") != nil{
+
+    }
+  })
+}
+
+
+
+func handleRrequests(){
+  htttp.Handle("/",isAuthorized(homePage))
+  lag.Fatal(http.ListenAndServe(":9001",nil)) //starting server
+}
+
+func main(){
+  fmt.Printf("Server")
+  handleRrequests()
+}
