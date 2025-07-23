7f029128edca9b4f5a6d6b309ee309b1f76e9970
vijaysb0613 <vijaysb2006@gmail.com>
2025-07-24 01:26:32 +0530

    Update main func with handlers

--- go/todo/cmd/server/main.go
+++ go/todo/cmd/server/main.go
@@ -4,6 +4,9 @@ import (
   "fmt"
   "net/http"
   "todo/internal/config"
+  "todo/internal/handler"
+  "todo/internal/repository"
+  "todo/internal/service"
 )
 
 func main() {
@@ -12,6 +15,9 @@ func main() {
   db := config.ConnectDB(cfg.DBUrl)
   defer db.Close()
   fmt.Println("Starting the Server on Port ",cfg.Port)
+  todoRepo := repository.NewTodoRepository(db)
+  todoSvc := service.NewTodoService(todoRepo)
+  todoHandler := handler.NewTodoHandler(todoSvc)
 
   http.HandleFunc("/health", func(w http.ResponseWriter,r *http.Request) {
     w.WriteHeader(http.StatusOK)
